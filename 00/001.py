7d81471647643840f1c7411bc2df79fcdfcc8adc
Devraj <devrajparmar232@gmail.com>
2026-05-02 14:57:40 +0530

    Initials

---
+++ 00/001.py
@@ -0,0 +1,26 @@
+# def sum(a,b):
+#     return a+b
+
+# print(sum(4,6))
+
+
+# name = "Priyanshi"
+# age = 21
+
+# print(f"{name} is very kind and supportive and her age is {age}")
+
+# name = "Priya"
+# age = 24
+# value = 8.4
+# is_available = False 
+
+# if is_available:
+#     print(f"{name} is present ")
+# else:
+#     print(f"{name} is not present ")
+
+# age = age // 2
+# print(age)
+
+name = input("Enter your wish name")
+print(f"Your wish name is  : {name}")

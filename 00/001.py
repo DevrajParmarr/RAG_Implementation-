5e562a6af09c14ce30584c59f815a75493bd4e80
Devraj <devrajparmar232@gmail.com>
2026-06-19 23:08:19 +0530

    01

--- 00/001.py
+++ 00/001.py
@@ -1,13 +1,14 @@
 # def sum(a,b):
 #     return a+b
 
-# print(sum(4,6))
+# a  = sum(6,6) 
+# print(a)
 
 
-# name = "Priyanshi"
+# name = "Pyanshi"
 # age = 21
 
-# print(f"{name} is very kind and supportive and her age is {age}")
+# print(f"{name} is very beautiful- kind and supportive and her age is {age}")
 
 # name = "Priya"
 # age = 24
@@ -15,12 +16,54 @@
 # is_available = False 
 
 # if is_available:
-#     print(f"{name} is present ")
+#     print(f"{name} is present ")ṇṇṇ
 # else:
 #     print(f"{name} is not present ")
 
 # age = age // 2
 # print(age)
 
-name = input("Enter your wish name")
-print(f"Your wish name is  : {name}")
+name = input("Enter your name")
+gf = input("Enter your gf name")
+
+print("Your name is : " + name + " and your gf name is : " + gf)
+
+# TODAYS TASKS 20-06-2024  ,
+#  10 DAY BEFORE JOINING THE COMPANY  THINGS I HAVE PREPARE BEFORE JOINING
+
+#      0.LINUX CAMMAND KA USED TO HONA HAI 
+# 1.	MASTER PYTHON (IMPLEMENTATION LEVEL )
+# 2.	MASTER PYTHON FRAMEWORK
+# 3.	ML ALGORITHM CONCEPT 
+# 4.	RECOMMENDATION ALGORITHM
+# 5.	LLM TRAINING – TUNNING 
+# 6.	SYSTEM DESIGN MASTER KARNA HAI 
+# 7.	LYBRARY CALLING KO BETTER KARNA HAI
+# 8.	BACKEND IN PYTHON MASTER KARNA HAI
+# 9.	MASTER PYTHON TO IMPLEMENTATION LEVEL
+
+# 20 /06/2026 – 21/06/2026
+# 10 DAY BEFORE JOINING
+# BEFORE EOD , I HAVE TO FINISH 4 VIDEOS – 1.PYTHON , NUMBY , PANDA , MATPLOT .
+
+# ALSO BASIC LINUX ENVIROMENT SE FAMILIAR RHNA HAI 
+
+# 1. introduction to python
+# 2. data types in python   
+# 3. variables in python
+# 4. input and output in python
+# 5. operators in python
+# 6. conditional statements in python
+# 7. loops in python
+# 8. functions in python
+
+
+#basic function in python
+
+# def sum(a,b):
+#     return a+b  
+
+# a  = sum(6,6)
+# print(a)
+
+

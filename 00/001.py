9a73e627c97cace98d9ab7bf3ce5c4be7bbe86d2
Devraj <devrajparmar232@gmail.com>
2026-06-30 11:12:03 +0530

    Practice1

--- 00/001.py
+++ 00/001.py
@@ -23,10 +23,10 @@
 # age = age // 2
 # print(age)
 
-name = input("Enter your name")
-gf = input("Enter your gf name")
+# name = input("Enter your name :")
+# gf = input("Enter your gf name :")
 
-print("Your name is : " + name + " and your gf name is : " + gf)
+# print("Your name is : " + name + " and your gf name is : " + gf)
 
 # TODAYS TASKS 20-06-2024  ,
 #  10 DAY BEFORE JOINING THE COMPANY  THINGS I HAVE PREPARE BEFORE JOINING
@@ -67,3 +67,91 @@ print("Your name is : " + name + " and your gf name is : " + gf)
 # print(a)
 
 
+#type casting in python ( one variable type to another variable type) , to know the type of variable we use type() function
+
+# print(type(5))
+# print(type(5.0))
+# print(type("5"))
+
+#use int() function to convert string to integer
+#int("5") this will convert string "5" to integer 5
+#use float() function to convert string to float
+#float
+#use str() function to convert integer or float to string
+#str(5) this will convert integer 5 to string "5"
+
+# if 5>2:
+#     print("5 is greater than 2")
+
+# if 2<5:
+#  print("2 is less than 5")
+
+# print("Hello World" , end =" ")    # this will print "Hello World" and the next print statement will be in the same line
+# print ("Hello World")
+
+# print("i am ",23 , "years old")
+
+# x , y, z = 5, 10, 15
+# print(x+y+z)
+# print(x*y*z)    
+
+# for i in range(5):
+#     print(i)
+
+# x = "awesome"
+
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+
+# myfunc()
+
+# print("Python is " + x)
+
+# x = range(5)
+# for n in x:
+#     print(n)
+
+# x = b"hello"
+# print(x)
+
+# x = 35e3
+# y = 12E4
+# z = -87.7e100
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# ''' 
+# random number ka khel 
+
+# '''
+
+# import random
+# print(random.randrange(1,10))  # this will print a random number between 1 and 10  
+
+txt = "The best things in life are free!"
+print("fred" not in txt)
+
+b = "Hello, World!"
+print(b[2:5])
+print(b[-5:])
+
+print(b.upper())
+print(b.lower())
+
+print(b.replace("e", "J"))
+print(b)
+
+print(b.split(" ")) 
+
+x =2 
+print(f'The price is {x:.2f} dollars')
+print(f'The price is {x:2} dollars')
+
+x = 5
+
+print(1 < x < 10)
+
+print(1 < x and x < 10)
